b33ba3ddb46ad031b9eb98d944258eb15d017cdb
Vi-Sri <nikola.tesla195@gmail.com>
2022-06-22 20:23:32 +0530

    add vcpkg support

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <sqlite3.h>
 
 int main() {
     int var = 10;
     int* const& ptr_ref = &var;
     std::cout<<*ptr_ref<<std::endl;
+    std::cout<<"Sqlite Version : "<<sqlite3_libversion()<<std::endl;
     return 0;
 }
